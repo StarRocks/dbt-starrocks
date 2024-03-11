350b853ce86fa97f65124ae14f68ed5e639217c6
alberttwong <atwong@alumni.uci.edu>
2024-03-11 16:19:29 -0700

    adding basic unit tests
    Signed-off-by: alberttwong <atwong@alumni.uci.edu>

---
+++ tests/conftest.py
@@ -0,0 +1,21 @@
+import pytest
+import os
+
+# Import the standard functional fixtures as a plugin
+# Note: fixtures with session scope need to be local
+pytest_plugins = ["dbt.tests.fixtures.project"]
+
+# The profile dictionary, used to write out profiles.yml
+# dbt will supply a unique schema per test, so we do not specify 'schema' here
+@pytest.fixture(scope="class")
+def dbt_profile_target():
+    return {
+        'type': 'starrocks',
+        'threads': 1,
+        'server': 'localhost',
+        'username': 'root',
+        'password': '',
+        'port': 9030,
+        'ssl_disabled': True,
+        'version': '3.1.9'
+    }
---
+++ tests/functional/adapter/test_basic.py
@@ -0,0 +1,51 @@
+import pytest
+
+from dbt.tests.adapter.basic.test_base import BaseSimpleMaterializations
+from dbt.tests.adapter.basic.test_singular_tests import BaseSingularTests
+from dbt.tests.adapter.basic.test_singular_tests_ephemeral import BaseSingularTestsEphemeral
+from dbt.tests.adapter.basic.test_empty import BaseEmpty
+from dbt.tests.adapter.basic.test_ephemeral import BaseEphemeral
+from dbt.tests.adapter.basic.test_incremental import BaseIncremental
+from dbt.tests.adapter.basic.test_generic_tests import BaseGenericTests
+from dbt.tests.adapter.basic.test_snapshot_check_cols import BaseSnapshotCheckCols
+from dbt.tests.adapter.basic.test_snapshot_timestamp import BaseSnapshotTimestamp
+from dbt.tests.adapter.basic.test_adapter_methods import BaseAdapterMethod
+
+class TestSimpleMaterializationsMyAdapter(BaseSimpleMaterializations):
+    pass
+
+
+class TestSingularTestsMyAdapter(BaseSingularTests):
+    pass
+
+
+class TestSingularTestsEphemeralMyAdapter(BaseSingularTestsEphemeral):
+    pass
+
+
+class TestEmptyMyAdapter(BaseEmpty):
+    pass
+
+
+class TestEphemeralMyAdapter(BaseEphemeral):
+    pass
+
+
+class TestIncrementalMyAdapter(BaseIncremental):
+    pass
+
+
+class TestGenericTestsMyAdapter(BaseGenericTests):
+    pass
+
+
+class TestSnapshotCheckColsMyAdapter(BaseSnapshotCheckCols):
+    pass
+
+
+class TestSnapshotTimestampMyAdapter(BaseSnapshotTimestamp):
+    pass
+
+
+class TestBaseAdapterMethod(BaseAdapterMethod):
+    pass
