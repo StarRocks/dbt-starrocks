2f7248fd472145e35827c08d1a9bbd680490ea0b
VasylRatushniuk <vasylrat@gmail.com>
2026-01-14 12:21:55 +0200

    Add minor tests
    Signed-off-by: VasylRatushniuk <vasylrat@gmail.com>

---
+++ tests/functional/adapter/test_key_type_validation.py
@@ -0,0 +1,53 @@
+import pytest
+import dbt.exceptions
+
+from dbt.tests.util import run_dbt
+
+invalid_model_sql = """
+{{ config(
+    materialized='table',
+    table_type='DUPLICATE',
+    keys=['hk_col'],
+    distributed_by=['hk_col'],
+    engine='OLAP'
+) }}
+select
+    cast(x'010121' as varbinary(16)) as hk_col
+"""
+
+valid_model_sql = """
+{{ config(
+    materialized='table',
+    table_type='DUPLICATE',
+    keys=['bigint_col'],
+    distributed_by=['bigint_col'],
+    engine='OLAP'
+) }}
+select
+    cast(123 as bigint) as bigint_col
+"""
+
+class TestBinaryKeyValidation:
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "binary_key_model.sql": invalid_model_sql,
+            "valid_key_model.sql": valid_model_sql,
+        }
+
+    def test_invalid_key_succeeds(self):
+        """
+        Expected behavior:
+        - dbt run FAILS with DbtRuntimeError
+        """
+        with pytest.raises(dbt.exceptions.DbtRuntimeError):
+            run_dbt(["run", "--select", "binary_key_model"])
+
+    def test_valid_key_succeeds(self):
+        """
+        Expected behavior:
+        - dbt run SUCCEEDS and table is created
+        """
+        results = run_dbt(["run", "--select", "valid_key_model"])
+        assert len(results) == 1
